7e8e1116a5f997b599f9e7891f3db13c2bf6b0d1
AProkh1 <113512743+AProkh1@users.noreply.github.com>
2023-03-18 15:09:26 +0300

    Add files via upload

---
+++ lab3/lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab3
+//
+//  Created by Антон Прохоров on 18/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
